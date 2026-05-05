ddcd0bd5e5d52eda40fdbc648cbefbacb636b080
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-05-05 07:36:39 +0530

    revamp and initialize the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import MapDashboard from "./components/Map";
+import { buildGraph } from "./utils/graphBuilder";
+import bhopalData from "./data/bhopalData.geojson";
 
 function App() {
+  const [roadGraph, setRoadGraph] = useState(null);
+
+  useEffect(() => {
+    console.log(`Building road network graph...`);
+    const graph = buildGraph(bhopalData);
+    setRoadGraph(graph);
+    console.log(`Graph built with ${Object.keys(graph).length} nodes.`);
+  }, []);
   return (
-    <>
-      <MapDashboard />
-    </>
+    <div classnName="App">
+      <MapDashboard graph={roadGraph} />
+    </div>
   );
 }
 
