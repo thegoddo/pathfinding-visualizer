eefa354fccf0ffa76f0418c47c96c7135c64df3b
Khusi Jain <Khushijain040502@gmail.com>
2026-05-05 14:33:34 +0530

    Fix: move geojson fetch to useEffect and handle async data

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,39 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import MapDashboard from "./components/Map";
 import { buildGraph } from "./utils/graphBuilder";
-import bhopalData from "./data/bhopalData.geojson";
+//import bhopalData from "./data/bhopalData.geojson";
 
+// now fetch the data from ./data/bhopalData.geojson" and then stored it to a variable for future use
+//cant use require as it shows not defined 
+
+ //const bhopalData = fetch("./data/bhopalData.geojson")
+  
+
+  
 function App() {
   const [roadGraph, setRoadGraph] = useState(null);
 
   useEffect(() => {
+
+    const loadData = async () => {
+      try {
+        console.log("Fetching GeoJSON data...");
+        // Ensure this file is in your 'public/data' folder if using Vite/CRA
+        const response = await fetch("./data/bhopalData.geojson");
+        const data = await response.json();
+
     console.log(`Building road network graph...`);
-    const graph = buildGraph(bhopalData);
+    const graph = buildGraph(data);
     setRoadGraph(graph);
     console.log(`Graph built with ${Object.keys(graph).length} nodes.`);
-  }, []);
+  }catch (error) {
+        console.error("Failed to load or parse GeoJSON:", error);
+      }
+    };
+    loadData();
+  },
+ []);
+ 
   return (
     <div classnName="App">
       <MapDashboard graph={roadGraph} />
